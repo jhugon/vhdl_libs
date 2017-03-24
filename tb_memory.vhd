8be36cd1b88e292d29a5d02a312c6f6ec313f0bf
Justin Hugon <jhugon@fnal.fake>
2017-03-24 15:42:32 -0500

    seems to work now

--- tb_memory.vhd
+++ tb_memory.vhd
@@ -15,12 +15,13 @@ architecture tb of tb_memory is
     signal clock : std_logic := '0';
     signal wenable : std_logic := '0';
     signal renable : std_logic := '0';
-    signal waddress : std_logic_vector(7 downto 0) := "00000000";
-    signal raddress : std_logic_vector(7 downto 0) := "00000000";
+    signal waddress : std_logic_vector(3 downto 0) := "0000";
+    signal raddress : std_logic_vector(3 downto 0) := "0000";
     signal data_in : std_logic_vector(7 downto 0) := "00000000";
     signal data_out : std_logic_vector(7 downto 0) := "00000000";
 begin
-  mem : entity work.two_port_memory port map (clock => clock,
+  mem : entity work.two_port_memory generic map (Ndata => 8, Naddress => 4) 
+                                 port map (clock => clock,
                                     wenable => wenable, renable => renable,
                                     waddress => waddress, raddress => raddress,
                                     data_in => data_in, data_out => data_out
@@ -31,23 +32,37 @@ begin
     begin
       wenable <= '0';
       renable <= '0';
-      for i in 0 to 255 loop
-        wait until rising_edge(clock);
+      raddress <= "0000";
+      wait until rising_edge(clock);
+      for i in 0 to 2**waddress'length -1 loop
         wenable <= '1';
         waddress <= std_logic_vector(to_unsigned(i,waddress'length));
-        data_in <= (others => '0');
+        data_in <= std_logic_vector(to_unsigned(i,data_in'length));
+        wait until rising_edge(clock);
       end loop;
       wenable <= '0';
       renable <= '0';
     end procedure fill_up_mem;
     procedure read_out_mem is
     begin
-      for i in 0 to 255 loop
-        wait until rising_edge(clock);
+      wenable <= '0';
+      renable <= '0';
+      waddress <= "0000";
+      raddress <= "0000";
+      renable <= '1';
+      wait until rising_edge(clock);
+      for i in 0 to 2**waddress'length -1 loop
         renable <= '1';
-        raddress <= std_logic_vector(to_unsigned(i,waddress'length));
+        if i < 2**waddress'length-1 then
+          raddress <= std_logic_vector(to_unsigned(i+1,waddress'length));
+        end if;
+        wait until rising_edge(clock);
         log("Memory address " & to_string(i) & ":" & to_string(data_out));
       end loop;
+      wenable <= '0';
+      renable <= '0';
+      waddress <= "0000";
+      raddress <= "0000";
     end procedure read_out_mem;
   begin
     logger_init(display_format => level);
@@ -59,11 +74,9 @@ begin
 
       if run("fill_up_mem") then
         fill_up_mem;
-        check(true,"Failing check");
       elsif run("fill_up_mem_read_out") then
         fill_up_mem;
         read_out_mem;
-        check(true,"Failing check");
       end if;
     end loop;
 
--- two_port_memory.vhd
+++ two_port_memory.vhd
@@ -2,19 +2,27 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+library vunit_lib;
+context vunit_lib.vunit_context;
+use vunit_lib.log_types_pkg.all;
+use vunit_lib.log_special_types_pkg.all;
+use vunit_lib.log_pkg.all;
+
 entity two_port_memory is
+    generic(Ndata : integer; -- data word bit width
+            Naddress : integer); -- address word bit width
     port(
         clock : in std_logic;
         wenable : in std_logic;-- write enable
         renable : in std_logic;-- output enable
-        waddress : in std_logic_vector(7 downto 0);
-        raddress : in std_logic_vector(7 downto 0);
-        data_in : in std_logic_vector(7 downto 0);
-        data_out : out std_logic_vector(7 downto 0));
+        waddress : in std_logic_vector(Naddress - 1 downto 0);
+        raddress : in std_logic_vector(Naddress - 1 downto 0);
+        data_in : in std_logic_vector(Ndata - 1 downto 0);
+        data_out : out std_logic_vector(Ndata - 1 downto 0));
 end two_port_memory;
 
 architecture behavioral of two_port_memory is
-    type mem_array is array (0 to 2**(waddress'length) - 1)
+    type mem_array is array (0 to 2**(Naddress) - 1)
     of std_logic_vector (7 downto 0);
 begin
     write_read: process(clock)
@@ -30,6 +38,13 @@ begin
             if wenable = '1' then
                 mem_v(to_integer(unsigned(waddress))) := data_in;
             end if;
+--            log("Two port memory at end of process:");
+--            log("wenable: " & to_string(wenable) & " renable: " & to_string(renable));
+--            log("waddress: " & to_string(waddress) & " raddress: " & to_string(raddress));
+--            log("data_in: " & to_string(data_in) & " data_out: " & to_string(data_out));
+--            for i in 0 to 2**waddress'length -1 loop
+--              log(to_string(mem_v(i)));
+--            end loop;
         end if; -- rising edge clock
     end process;
 end behavioral;
