9379f0b6178f3c79612bf4b6a652ce9d177c3c70
Justin Hugon <jhugon@fnal.fake>
2017-03-28 11:22:55 -0500

    circular_buffer_controller: first draft

--- circular_buffer_controller.vhd
+++ circular_buffer_controller.vhd
@@ -8,6 +8,7 @@ use vunit_lib.log_types_pkg.all;
 use vunit_lib.log_special_types_pkg.all;
 use vunit_lib.log_pkg.all;
 
+-- ***USER'S RESPONSIBILITY TO NOT READ WHEN EMPTY***
 entity circular_buffer_controller is
     generic(Ndata : integer; -- data word bit width
             Naddress : integer); -- address word bit width
@@ -44,7 +45,36 @@ begin
     end process;
     -- next state
     process(wenable,renable,waddress,raddress,full,empty)
+      variable raddress_num : unsigned(Naddress - 1 downto 0);
+      variable waddress_num : unsigned(Naddress - 1 downto 0);
     begin
+      raddress_num := unsigned(raddress);
+      waddress_num := unsigned(waddress);
+      if wenable and renable then
+        raddress_next <= std_logic_vector(raddress_num + 1);
+        waddress_next <= std_logic_vector(waddress_num + 1);
+        full_next <= full;
+      elsif wenable then
+        waddress_next <= std_logic_vector(waddress_num + 1);
+        if raddress_num = waddress_num then
+          raddress_next <= std_logic_vector(raddress_num + 1);
+          full_next <= '1';
+        elsif raddress_num = (waddress_num + 1) then
+          raddress_next <= raddress;
+          full_next <= '1';
+        else 
+          raddress_next <= raddress;
+          full_next <= '0';
+        end if;
+      elsif renable then
+        raddress_next <= std_logic_vector(raddress_num + 1);
+        waddress_next <= waddress;
+        full_next <= '0';
+      else -- not wenable or renable
+        waddress_next <= waddress;
+        raddress_next <= raddress;
+        full_next <= full;
+      end if; -- wenable renable ifs
     end process;
     -- output
     empty <= '1' when ((waddress = raddress) and (not full)) else '0';
