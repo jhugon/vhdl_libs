764488af186fdadfa1533c623e11c772f64fd522
Justin Hugon <justin@fake.com>
2017-03-30 20:34:19 -0500

    tb_readout_controller.vhd added

---
+++ tb_readout_controller.vhd
@@ -0,0 +1,62 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+library vunit_lib;
+context vunit_lib.vunit_context;
+use vunit_lib.log_types_pkg.all;
+use vunit_lib.log_special_types_pkg.all;
+use vunit_lib.log_pkg.all;
+
+entity tb_readout_controller is
+  generic (runner_cfg : string);
+end entity;
+
+architecture tb of tb_readout_controller is
+    signal clock : std_logic := '0';
+    signal reset : std_logic := '0';
+    signal wenable : std_logic := '0';
+    signal renable : std_logic := '0';
+    signal full : std_logic := '0';
+    signal empty : std_logic := '0';
+    signal dead : std_logic := '0';
+    signal trigger : std_logic := '0';
+begin
+  cbc : entity work.readout_controller port map (clock => clock, reset => reset,
+                                    wenable => wenable, renable => renable,
+                                    full => full, empty => empty,
+                                    dead => dead, trigger => trigger
+                                 );
+  main : process
+    procedure reset_entity is
+    begin
+        full <= '0';
+        empty <= '0';
+        trigger <= '0';
+        reset <= '1';
+        wait for 50 ns; -- check reset
+        reset <= '0';
+    end procedure reset_entity;
+  begin
+    logger_init(display_format => level);
+    --logger_init(display_format => verbose);
+    --checker_init(display_format => level);
+    checker_init(display_format => verbose);
+    --enable_pass_msg;
+    test_runner_setup(runner, runner_cfg);
+
+    while test_suite loop
+
+      if run("reset") then
+        reset_entity;
+        check_equal(dead,true,"dead");
+        check_equal(wenable,true,"wenable");
+        check_equal(renable,false,"renable");
+      end if;
+    end loop;
+
+    test_runner_cleanup(runner);
+  end process;
+
+  clock <= not clock after 25 ns;
+  test_runner_watchdog(runner, 1 ms);
+end architecture;
