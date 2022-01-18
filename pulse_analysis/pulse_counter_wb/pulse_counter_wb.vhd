af2301a5fe1313381e401e3354a3c8a7e3e1e827
Justin Hugon <jhugon@users.noreply.github.com>
2022-01-18 13:04:17 -0700

    pulse_counter_wb: working, but needs more testing
      Need to look at gtkwave to see if waveforms match wb spec
      Need to add more tests, especially reading count near a pulse

--- pulse_analysis/pulse_counter_wb/pulse_counter_wb.vhd
+++ pulse_analysis/pulse_counter_wb/pulse_counter_wb.vhd
@@ -5,9 +5,8 @@ use ieee.math_real.all;
 
 -- sel_i(0) is read only and the current count
 -- sel_i(1) is the control/status register. Bit 0 is enable, bit 1 is reset.
+-- counter and data bus are both 32 bits wide
 entity pulse_counter_wb is
-    generic(Nbits: integer := 10; -- size of counter
-            wb_width: integer := 32); -- width of wishbone data bus
     port(
         sig_in : in std_logic;
         wb_clk_i : in std_logic;
@@ -16,9 +15,9 @@ entity pulse_counter_wb is
         wb_sel_i : in std_logic_vector(1 downto 0);
         wb_stb_i : in std_logic;
         wb_we_i : in std_logic;
-        wb_dat_i : in std_logic_vector(wb_width -1 downto 0);
+        wb_dat_i : in std_logic_vector(31 downto 0);
         wb_ack_o : out std_logic;
-        wb_dat_o : out std_logic_vector(wb_width -1 downto 0)
+        wb_dat_o : out std_logic_vector(31 downto 0)
         );
 end;
 
@@ -35,54 +34,62 @@ architecture behavioral of pulse_counter_wb is
     end component;
     signal enable_reg : std_logic;
     signal enable_next : std_logic;
-    signal reset : std_logic;
-    signal count : std_logic_vector(Nbits -1 downto 0);
+    signal reset_reg : std_logic;
+    signal reset_next : std_logic;
+    signal count : std_logic_vector(31 downto 0);
+    signal actually_reset : std_logic;
 begin
     pls_counter : pulse_counter 
-        generic map (Nbits => Nbits)
+        generic map (Nbits => 32)
         port map (
             clock => wb_clk_i,
-            reset => reset,
+            reset => actually_reset,
             enable => enable_reg,
             count => count,
             sig_in => sig_in
         );
     -- registers
-    process(wb_clk_i)
+    reg_proc : process(wb_clk_i)
     begin
         if rising_edge(wb_clk_i) then
-            if reset = '1' then
+            if actually_reset = '1' then
                 enable_reg <= '0';
+                reset_reg <= '0';
             else
                 enable_reg <= enable_next;
+                reset_reg <= reset_next;
             end if;
         end if;
     end process;
     -- next state
-    process(wb_stb_i,wb_we_i,wb_sel_i,wb_dat_i,wb_rst_i,enable_reg,count,reset)
+    next_state_proc : process(wb_stb_i,wb_we_i,wb_sel_i,wb_dat_i,wb_rst_i,enable_reg,count,reset_reg)
     begin
         if wb_stb_i = '1' then
             if wb_we_i = '1' then
                 wb_dat_o <= (others => '0');
                 if wb_sel_i(1) = '1' then
                     enable_next <= wb_dat_i(0);
-                    reset <= wb_dat_i(1) or wb_rst_i;
+                    reset_next <= wb_dat_i(1);
                 else
                     enable_next <= enable_reg;
-                    reset <= wb_rst_i;
+                    reset_next <= reset_reg;
                 end if;
             else
                 enable_next <= enable_reg;
-                reset <= wb_rst_i;
+                reset_next <= reset_reg;
                 if wb_sel_i(1) = '1' then
-                    wb_dat_o <= (0 => enable_reg, 1 => reset, others => '0');
+                    wb_dat_o <= (0 => enable_reg, 1 => reset_reg, others => '0');
                 else
                     wb_dat_o <= count;
                 end if;
             end if;
         else
+            enable_next <= enable_reg;
+            reset_next <= reset_reg;
+            wb_dat_o <= (others => '0');
         end if;
     end process;
+    actually_reset <= reset_reg or wb_rst_i;
     -- output
     wb_ack_o <= wb_stb_i;
 end behavioral;
