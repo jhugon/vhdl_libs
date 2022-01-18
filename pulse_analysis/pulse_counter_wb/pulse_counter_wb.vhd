1462021aecfe1d14c2b3f2a5ff7161a6b3af0f74
Justin Hugon <jhugon@users.noreply.github.com>
2022-01-17 20:10:14 -0700

    added pulse_counter_wb, doesn't yet compile

---
+++ pulse_analysis/pulse_counter_wb/pulse_counter_wb.vhd
@@ -0,0 +1,88 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+-- sel_i(0) is read only and the current count
+-- sel_i(1) is the control/status register. Bit 0 is enable, bit 1 is reset.
+entity pulse_counter_wb is
+    generic(Nbits: integer := 10; -- size of counter
+            wb_width: integer := 32); -- width of wishbone data bus
+    port(
+        sig_in : in std_logic;
+        wb_clk_i : in std_logic;
+        wb_rst_i : in std_logic;
+        wb_cyc_i : in std_logic;
+        wb_sel_i : in std_logic_vector(1 downto 0);
+        wb_stb_i : in std_logic;
+        wb_we_i : in std_logic;
+        wb_dat_i : in std_logic_vector(wb_width -1 downto 0);
+        wb_ack_o : out std_logic;
+        wb_dat_o : out std_logic_vector(wb_width -1 downto 0)
+        );
+end;
+
+architecture behavioral of pulse_counter_wb is
+    component pulse_counter is
+        generic(Nbits: integer := 10); -- size of counter
+        port(
+            clock : in std_logic;
+            reset : in std_logic;
+            enable : in std_logic;
+            sig_in : in std_logic;
+            count : out std_logic_vector(Nbits -1 downto 0)
+            );
+    end component;
+    signal enable_reg : std_logic;
+    signal enable_next : std_logic;
+    signal reset : std_logic;
+    signal count : std_logic_vector(Nbits -1 downto 0);
+begin
+    pls_counter : pulse_counter 
+        generic map (Nbits => Nbits)
+        port map (
+            clock => wb_clk_i,
+            reset => reset,
+            enable => enable_reg,
+            count => count,
+            sig_in => sig_in
+        );
+    -- registers
+    process(wb_clk_i)
+    begin
+        if rising_edge(wb_clk_i) then
+            if reset = '1' then
+                enable_reg <= '0';
+            else
+                enable_reg <= enable_next;
+            end if;
+        end if;
+    end process;
+    -- next state
+    process(wb_stb_i,wb_we_i,wb_sel_i,wb_dat_i,wb_rst_i,enable_reg,count,reset)
+    begin
+        if wb_stb_i = '1' then
+            if wb_we_i = '1' then
+                wb_dat_o <= (others => '0');
+                if wb_sel_i(1) = '1' then
+                    enable_next <= wb_dat_i(0);
+                    reset <= wb_dat_i(1) or wb_rst_i;
+                else
+                    enable_next <= enable_reg;
+                    reset <= wb_rst_i;
+                end if;
+            else
+                enable_next <= enable_reg;
+                reset <= wb_rst_i;
+                if wb_sel_i(1) = '1' then
+                    wb_dat_o <= reset & enable_reg & (others => '0');
+                else
+                    wb_dat_o <= count;
+                end if;
+            end if;
+        else
+        end if;
+    end process;
+    -- output
+    wb_ack_o <= wb_stb_i;
+end behavioral;
---
+++ pulse_analysis/pulse_counter_wb/test_pulse_counter_wb.py
@@ -0,0 +1,70 @@
+import cocotb
+from cocotb.triggers import Timer
+from cocotb.triggers import FallingEdge
+from cocotb.clock import Clock
+from cocotb.handle import ModifiableObject
+from cocotb.utils import get_sim_time
+from cocotbnumpy.test import NumpyTest
+from cocotbnumpy.signal import NumpySignal
+import numpy as np
+
+def model(inputs):
+    reset = inputs["reset"]
+    sig_in = inputs["sig_in"]
+    enable = inputs["enable"]
+    N = len(sig_in)
+    result = NumpySignal(np.zeros(N),[1])
+    count = 0
+    for i in range(2,N):
+        rising_edge = 0
+        if sig_in[i-3] == 0 and sig_in[i-2] == 1 and reset[i-2] != 1:
+            rising_edge = 1
+        if reset[i-1]:
+            count = 0
+        else:
+            count += rising_edge*enable[i-1]
+        result[i] = count
+    exp = {
+        "count": result
+    }
+    return exp
+
+@cocotb.test()
+async def pulse_counter_test(dut):
+    ## Test pulses and enable
+    N = 40
+    pulses_at = [5,6,7,9,15,20,30,35]
+    inputs = {
+        "reset": np.zeros(N),
+        "enable": np.ones(N),
+        "sig_in": np.zeros(N),
+    }
+
+    inputs["reset"][:1] = 1
+    inputs["sig_in"][pulses_at] = 1
+    inputs["enable"][31] = 0 # diable for pulse at 30
+
+    exp = model(inputs)
+
+    nptest = NumpyTest(dut,inputs,exp,"clock")
+    await nptest.run()
+
+    ## Test pulses and reset
+
+    N = 38
+    pulses_at = [5,10,15,20,25,30,35]
+    inputs = {
+        "reset": np.zeros(N),
+        "enable": np.ones(N),
+        "sig_in": np.zeros(N),
+    }
+
+    inputs["reset"][:1] = 1
+    inputs["reset"][[4,10,16,22]] = 1
+    inputs["sig_in"][pulses_at] = 1
+    inputs["enable"][31] = 0 # diable for pulse at 30
+
+    exp = model(inputs)
+
+    nptest = NumpyTest(dut,inputs,exp,"clock")
+    await nptest.run()
