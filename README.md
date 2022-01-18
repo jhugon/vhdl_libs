416081d9a7236d699c0a70326f8c5588b5bf42dc
Justin Hugon <jhugon@users.noreply.github.com>
2022-01-18 16:31:05 -0700

    uart_tx: implemented, but not tested

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Learn VHDL
 
-Uses python cocotb package to write test/validation in python. Use the LLVM backend to GHDL, it's easier.
+Uses python cocotb package to write test/validation in python.
 
 ## Installation
 
+ Use the LLVM backend to GHDL, it's easier than GCC and works well.
+
     sudo apt-get install make gcc g++ python3 python3-dev python3-pip iverilog ghdl-llvm
+
+On Ubuntu 20.04, the ghdl version doesn't work with VHDL-2008, which is needed
+for uart_tx. You may need to install from source. See
+https://ghdl.github.io/ghdl/development/building/LLVM.html#build-llvm for
+instructions.
 
 In a python virtualenv:
 
--- uart/uart_tx/uart_tx.vhd
+++ uart/uart_tx/uart_tx.vhd
@@ -7,14 +7,14 @@ entity uart_tx is
     generic(clock_freq: integer := 100000000; -- in hertz
             uart_freq: integer := 9600; -- in hertz
             uart_period_n_clock : integer := clock_freq/uart_freq; -- don't mess with this
-            timer_bit_width: integer := integer(ceil(log2(real(uart_period_n_clock))))*10); -- don't mess with this
+            timer_bit_width: integer := integer(ceil(log2(real(uart_period_n_clock*10))))); -- don't mess with this
     port(
         clock : in std_logic;
         reset : in std_logic;
-        data_good : in std_logic; -- pulse high to latch data and start sending
-        data : in std_logic_vector(7 downto 9);
+        latch_in_data : in std_logic; -- pulse high to latch data and start sending
+        in_data : in std_logic_vector(7 downto 0);
         tx : out std_logic;
-        sending : out std_logic -- tx in process, don't update data or put data_good high if this is high
+        sending : out std_logic -- tx in process, don't update in_data or put latch_in_data high if this is high
     );
 end uart_tx;
 
@@ -30,16 +30,21 @@ architecture behavioral of uart_tx is
         );
     end component;
 
+    type line_state_type is (IDLE_BIT,START_BIT,DATA_BIT,STOP_BIT);
+    signal line_state : line_state_type;
+    signal next_line_state : line_state_type;
+
     signal data_reg : std_logic_vector(7 downto 0);
     signal next_data_reg : std_logic_vector(7 downto 0);
-    signal sending_reg : std_logic;
-    signal next_sending_reg : std_logic;
+
     signal tx_reg : std_logic;
     signal next_tx_reg : std_logic;
+    signal ibit_reg : std_logic_vector(2 downto 0);
+    signal next_ibit_reg : std_logic_vector(2 downto 0);
 
     signal timer_count : std_logic_vector(timer_bit_width-1 downto 0);
     signal timer_reset : std_logic;
-    signal timer_max_value : std_logic_vector(timer_bit_width-1 downto 0) := std_logic_vector(unsigned(uart_period_n_clock));
+    constant timer_max_value : std_logic_vector(timer_bit_width-1 downto 0) := std_logic_vector(to_unsigned(uart_period_n_clock,timer_bit_width));
     signal timer_enable : std_logic;
 begin
     -- components
@@ -56,18 +61,80 @@ begin
     process(clock)
     begin
         if rising_edge(clock) then
+            if reset then
+                line_state <= IDLE_BIT;
+            else
+                line_state <= next_line_state;
+            end if;
+            -- not resettable
             data_reg <= next_data_reg;
-            sending_reg <= next_sending_reg;
+            ibit_reg <= next_ibit_reg;
         end if;
     end process;
     -- bookkeeping
     -- next state
-    next_data_reg <= data when data_good = '1' and sending_reg = '0' else data_reg;
-    next_sending_reg <= '0';
-    next_tx_reg <= '0';
+    process(all)
+        variable timer_max_value_int : integer;
+        variable timer_count_int : integer;
+        variable ibit_reg_int : integer;
+    begin
+        timer_max_value_int := to_integer(unsigned(timer_max_value));
+        timer_count_int := to_integer(unsigned(timer_count));
+        ibit_reg_int := to_integer(unsigned(ibit_reg));
+        case line_state is
+            when IDLE_BIT => 
+                if latch_in_data then
+                    next_line_state <= START_BIT;
+                    next_data_reg <= in_data;
+                else
+                    next_line_state <= line_state;
+                    next_data_reg <= data_reg;
+                end if;
+                next_tx_reg <= '1';
+                next_ibit_reg <= 3d"0";
+            when START_BIT => 
+                if timer_count = timer_max_value then
+                   next_line_state <= DATA_BIT;
+                else
+                   next_line_state <= line_state;
+                end if;
+                next_tx_reg <= '0';
+                next_ibit_reg <= 3d"0";
+                next_data_reg <= data_reg;
+            when DATA_BIT => 
+                if timer_count = timer_max_value then
+                    -- this is the shift register
+                    next_data_reg <= '0' & data_reg(data_reg'high downto data_reg'low+1);
+                    if ibit_reg = 3d"7" then
+                        next_line_state <= STOP_BIT;
+                        next_ibit_reg <= ibit_reg;
+                    else
+                        next_line_state <= line_state;
+                        next_ibit_reg <= std_logic_vector(to_unsigned(ibit_reg_int+1,3));
+                    end if;
+                else
+                    next_line_state <= line_state;
+                    next_ibit_reg <= ibit_reg;
+                    next_data_reg <= data_reg;
+                end if;
+                next_tx_reg <= data_reg(data_reg'low);
+            when STOP_BIT => 
+                if timer_count = timer_max_value then
+                   next_line_state <= IDLE_BIT;
+                else
+                   next_line_state <= line_state;
+                end if;
+                next_tx_reg <= '1';
+                next_ibit_reg <= 3d"0";
+                next_data_reg <= data_reg;
+        end case;
+    end process;
+    timer_reset <= '1' when line_state = IDLE_BIT else '0';
+    timer_enable <= '1' when line_state /= IDLE_BIT else '0';
     -- output
-    sending <= sending_reg;
+    sending <= '1' when line_state /= IDLE_BIT else '0';
     tx <= tx_reg;
     -- assertion
-    assert timer_bit_width = integer(ceil(log2(real(clock_freq/uart_freq))))*10;
+    assert uart_period_n_clock = clock_freq/uart_freq;
+    assert timer_bit_width = integer(ceil(log2(real(uart_period_n_clock*10))));
 end behavioral;
