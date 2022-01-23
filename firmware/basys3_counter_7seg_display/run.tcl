c83239b8a527d0f3c19069e50a11bcfb50fe193a
Justin Hugon <jhugon@users.noreply.github.com>
2022-01-22 17:41:40 -0700

    7seg counter fw: fixed vivado compilation
      Some changes to run.tcl to include all necessary modules
      I realized I was overusing integer (which truncates to 32 bits)
            fixed that in simple_timer
      Toward #18

--- firmware/basys3_counter_7seg_display/run.tcl
+++ firmware/basys3_counter_7seg_display/run.tcl
@@ -27,10 +27,9 @@ file mkdir $outputDir
 #
 #read_vhdl -library bftLib [ glob ./Sources/hdl/bftLib/*.vhdl ]         
 #read_verilog  [ glob ./Sources/hdl/*.v ]
-read_vhdl [ glob ../../reset_controller/*/*.vhd ]
+read_vhdl [ glob ../../misc/*/*.vhd ]
 read_vhdl [ glob ../../timers/*/*.vhd ]
-read_vhdl [ glob ../../switches/*/*.vhd ]
-read_vhdl [ glob ../../edges/*/*.vhd ]
+read_vhdl [ glob ../../seven_seg_display/*/*.vhd ]
 read_vhdl ./top.vhd
 read_xdc ./io_constraints.xdc
 #
