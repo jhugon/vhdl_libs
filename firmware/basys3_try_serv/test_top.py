120f9bce8cc0f8f250b9c99ea4c82f4aa4b0a4a0
Justin Hugon <jhugon@users.noreply.github.com>
2022-01-25 19:06:15 -0700

    Trying to get serv going

--- firmware/basys3_try_serv/test_top.py
+++ firmware/basys3_try_serv/test_top.py
@@ -1,6 +1,6 @@
 import cocotb
 from cocotb.triggers import Timer
-from cocotb.triggers import FallingEdge
+from cocotb.triggers import FallingEdge, Edge
 from cocotb.clock import Clock
 from cocotb.utils import get_sim_time
 from cocotbnumpy.test import NumpyTest
@@ -9,39 +9,25 @@ import numpy as np
 
 def model(inputs,N):
 
-    btnC = inputs["btnC"]
-    btnU = inputs["btnU"]
+    q = np.zeros(N,dtype=np.uint32)
 
-    count = np.zeros(N,dtype=np.uint32)
-
-    c = 0
-    for i in range(N):
-        if int(btnC[i-9]) == 0 and int(btnC[i-8]) == 1:
-            c += 1
-        if int(btnU[i-5]) == 0 and int(btnU[i-4]) == 1:
-            c += 1
-        count[i] = c
-
-    return {"count":count}
+    return {"q":q}
 
 @cocotb.test()
 async def fw_try_serv_test(dut):
-    """
-    Dummy test, b/c real hardware is the test of the display
-    """
     ## Test with a simple byte
-    N = 100
+    N = 1000
     inputs = {
-        "btnC": np.zeros(N,dtype=np.uint32),
-        "btnU": np.zeros(N,dtype=np.uint32),
+        "wb_rst": np.zeros(N,dtype=np.uint32),
     }
 
-    inputs["btnC"][10:15] = 1
-    inputs["btnU"][20:25] = 1
-    #inputs["btnC"][[30,32,34]] = 1
-    #inputs["btnU"][[36,38,40]] = 1
+    inputs["wb_rst"][0:100] = 1
 
     exp = model(inputs,N)
 
-    nptest = NumpyTest(dut,inputs,exp,"clk")
-    await nptest.run()
+    nptest = NumpyTest(dut,inputs,exp,"wb_clk")
+    await nptest.run(True)
+
+    for i in range(10):
+        await Edge(dut.q)
+        print(f"At {get_sim_time('ns'):10.0f} ns q: {dut.q.value}")
